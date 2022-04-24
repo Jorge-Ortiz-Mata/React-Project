a2f17da48e868de650c339a3d153be70df379e9c
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-04-24 18:05:27 -0500

    Adding a list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jorge Ortiz</h1>
+      <h3>Jr. Backend Developer at Orchata.</h3>
+      <p>Web developer, teacher and entrepreneur.</p>
+      <h4>Skills.</h4>
+      <ul>
+        <li />Ruby on Rails.
+        <li />Javascript.
+        <li />Ruby.
+        <li />React.
+      </ul>
     </div>
   );
 }
